eca64dcfb393959f787f74db563648651071e4ac
Tony Pham <mrphamtony@gmail.com>
2013-09-02 18:44:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Postup::Application.config.secret_key_base = '2dfa1267df8cc96555e648dc30b05131abf31678da8476e43b152e5dc9801d6d94a1930c52540621faa56b77b88dd912b3797bc22406fc6cba81a77f2f9e6542'
