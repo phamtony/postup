760bf92802843bd5dc86a6954207eaac2bc817f4
Tony Pham <mrphamtony@gmail.com>
2013-09-06 23:11:01 -0400

    Added a delete function to the comment section

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   
   mount_uploader :picture, PictureUploader
   
-  has_many :comments
+  has_many :comments, dependent: :destroy
   
   validate :name, presence: true,
                    length: { minimum: 5 }
